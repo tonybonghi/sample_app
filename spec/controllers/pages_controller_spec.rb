9b000db5687f4552461979c1e25763f960574f04
Tony Bonghi <tonybonghi@gmail.com>
2012-03-03 16:36:46 -0800

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -5,21 +5,24 @@ describe PagesController do
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
-      response.should be_success
+      response.should have_selector("title", 
+                  :content => "RoR - Home")
     end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
-      response.should be_success
+      response.should have_selector("title", 
+                  :content => "RoR - Contact")
     end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
-      response.should be_success
+      response.should have_selector("title", 
+                  :content => "RoR - About")
     end
   end
 
